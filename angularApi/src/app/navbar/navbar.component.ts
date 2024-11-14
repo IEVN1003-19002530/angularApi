59372e03a589e6b3b2c482d40482ca6bfa4538ce
Jorge Ivan Valadez Ontiveros <vanyvanxd18@gmail.com>
2024-11-13 23:23:04 -0600

    Servicios Terminado

--- angularApi/src/app/navbar/navbar.component.ts
+++ angularApi/src/app/navbar/navbar.component.ts
@@ -6,7 +6,6 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
